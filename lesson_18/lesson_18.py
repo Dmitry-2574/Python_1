de798400855d0d450f0e71514c441f9f573fd8cf
Dim <d@mal>
2024-12-12 22:58:02 +0700

    сделали лист

--- lesson_18/lesson_18.py
+++ lesson_18/lesson_18.py
@@ -2,3 +2,22 @@
 # YMAL
 import yaml
 
+shopping_list = [
+    {        "name": "Молоко",
+        "quantity": 2,
+        "unit": "литра",
+        "category": "Молочные продукты",
+        "priority": "высокая"    },    {        "name": "Хлеб",
+        "quantity": 1,
+        "unit": "буханка",
+        "category": "Хлебобулочные изделия", 
+        "priority": "средняя"    },    {        "name": "Яблоки",
+        "quantity": 1.5,
+        "unit": "кг",
+        "category": "Фрукты",
+        "priority": "низкая"
+    }
+]
+yaml_string = yaml.dump(shopping_list, allow_unicode=True)
+print(yaml_string)
+
